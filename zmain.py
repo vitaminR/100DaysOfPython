e5a2974cf3fae4be38d43e97b1ee93522938b9e7
vitaminR <nymilitarypolice@gmail.com>
2024-01-28 00:58:39 -0400

    .

--- zmain.py
+++ zmain.py
@@ -1,7 +1,27 @@
-name = "Johnathon"
-new_list = [letter for letter in name]
+# name = "Johnathon"
+# new_list = [letter for letter in name]
 
-print(new_list)
+# print(new_list)
 
-double = [n * 2 for n in range(1, 5)]
-print(double)
+# double = [n * 2 for n in range(1, 5)]
+# print(double)
+
+
+# # list conprehension with conditional
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+# short_names = [name for name in names if len(name) < 5]
+
+# print(short_names)
+
+# Open both files and read their content
+with open("file1.txt", "r") as file:
+    file1_content = file.read().splitlines()
+
+with open("file2.txt", "r") as file:
+    file2_content = file.read().splitlines()
+
+# Find common numbers
+common_numbers = [num for num in file1_content if num in file2_content]
+
+print(f"Common numbers: {common_numbers}")
