efaa2d86f488144539a0beb4aa19543fb9dfb177
vitaminR <nymilitarypolice@gmail.com>
2024-02-03 22:23:14 -0400

    .

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,38 +1,13 @@
 import pandas as pd
 
 
-class Phonetic:
-    def __init__(self, csv_file):
-        self.phonetic = pd.read_csv(csv_file)
-        self.phonetic_dict = {key: value for (key, value) in self.phonetic.items()}
+def get_phonetic_dict(csv_file):
+    # Read the CSV file into a pandas DataFrame
+    phonetic = pd.read_csv(csv_file)
+    # Convert the DataFrame into a dictionary where the keys are the letters and the values are the codes
+    return {phonetic.letter[i]: phonetic.code[i] for i in range(len(phonetic))}
 
 
-# student_data_frame = pd.DataFrame(student_dict)
+user_input = "Give me a word to spell phonetically:\n"
 
-# # Loop through rows of a data frame
-# for index, row in student_data_frame.iterrows():
-#     # Access index and row
-#     # Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# TODO 1. Create a dictionary in this format:
-phon_alph = Phonetic("nato_phonetic_alphabet.csv")
-
-# for key, value in phon_alph.phonetic_dict.items():
-#     # Your code here
-
-
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-
-# print(student_data_frame)
-print(phon_alph.phonetic_dict.items())
-
-
-# # Looping through dictionaries:
-# for key, value in student_dict.items():
-#     # Access key and value
-#     pass
+print(get_phonetic_dict("nato_phonetic_alphabet.csv"))
