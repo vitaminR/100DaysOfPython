ee6ce6cd64392faadafa74e63b167d99fa8ebcbd
vitaminR <nymilitarypolice@gmail.com>
2024-03-29 22:46:09 -0300

    Day32 starting project

--- Day32_Birthday-Wisher/main.py
+++ Day32_Birthday-Wisher/main.py
@@ -1,4 +1,12 @@
 import smtplib
 
 my_email = "nymilitarypolice@gmail.com"
-password = "AAron37$$"
+their_email = "networkdoctor@live.com"
+password = "bgkw fsho zqhl gkll"
+their_message = "Subject:Hello\n\nThis is the body of my email."
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs=their_email, msg=their_message)
+connection.close()
